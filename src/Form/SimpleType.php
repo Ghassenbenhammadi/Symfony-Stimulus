a42a7408c87e401aad3f7ca0a38baefd7bd8a5be
Ghassen benhammadi <benhammdighassen@gmail.com>
2023-07-20 12:16:31 +0200

    first commit

---
+++ src/Form/SimpleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Choix;
+use App\Entity\Simple;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SimpleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('choix', SimpleChoixAutocompleteField::class
+            // EntityType::class ,[
+            //     'class' => Choix::class,
+            //     'choice_label' => 'nom',
+            //     'placeholder'=> 'Choisissez un nom dans la liste',
+            //     'autocomplete' => true
+            // ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Simple::class,
+        ]);
+    }
+}
